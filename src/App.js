bf7b89c038da951a52c5aa9c0da4e427da8f3a3f
rabee <rabee.dameer@gmail.com>
2018-09-07 11:52:11 +0200

    added anew component 'CartList' to display the items added to the cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import ProductList from './components/ProductList';
+import CartList from './components/CartList';
 class App extends Component {
   render() {
     return (
+      <Fragment>
       <ProductList />
+      <CartList />
+      </Fragment>
     );
   }
 }
